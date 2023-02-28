c6004df42161f310ddd6fc7fb31d57a1882d4ea2
Bnttrang2002 <125441880+Bnttrang2002@users.noreply.github.com>
2023-02-28 23:44:25 +0700

    Update cau4.dart

--- cau4.dart
+++ cau4.dart
@@ -2,9 +2,8 @@ import 'dart:io';
 void dientichht(int r){
   print('${r*r*3.14}');
 }
-
 void main(){
-  stdout.write('Moi ban nhap ban kinh hinh tron : ');
+  stdout.write('Mời bạn nhập bán kính hình tròn : ');
   int r = int.parse(stdin.readLineSync()!);
   dientichht(r);
 }
