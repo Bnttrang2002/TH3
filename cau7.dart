a6d68ff4f80fea700b432ae7b4fe63bdb59235e2
Bnttrang2002 <125441880+Bnttrang2002@users.noreply.github.com>
2023-02-28 23:46:06 +0700

    Update cau7.dart

--- cau7.dart
+++ cau7.dart
@@ -1,17 +1,15 @@
 import 'dart:math';
 import 'dart:io';
-
 int luythua(int a, int b){
   var kq ;
   kq = pow(a, b);
   return kq;
 }
-
 void main(){
-  stdout.write('Moi ban nhap a: ');
+  stdout.write('Mời bạn nhập a: ');
   int a = int.parse(stdin.readLineSync()!);
-  stdout.write('Moi ban nhap b: ');
+  stdout.write('Mời bạn nhập b: ');
   int b = int.parse(stdin.readLineSync()!);
   var kq = luythua(a,b);
-  print('Ket qua la : $kq');
+  print('Kết quả là : $kq');
 }
